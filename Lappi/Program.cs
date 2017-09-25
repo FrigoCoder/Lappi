ae713e81b1db8d7d8cc8fc388b6b30ee004d0b81
Frigo <frigocoder@gmail.com>
2017-09-25 22:54:35 +0200

    Lappi: Created new solution and project.

---
+++ Lappi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lappi {
+
+    public static class Program {
+
+        [STAThread]
+        private static void Main () {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
+        }
+
+    }
+
+}
