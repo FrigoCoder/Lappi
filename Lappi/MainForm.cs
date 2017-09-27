eea17675c850ffa15e83264070993a8832cadae8
Frigo <frigocoder@gmail.com>
2017-09-27 21:54:23 +0200

    Lapp: Added a simple Lenna.png output.

---
+++ Lappi/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lappi {
+
+    public class MainForm : Form {
+
+        public MainForm () {
+            AutoSize = true;
+            Controls.Add(new PictureBox {
+                AutoSize = true,
+                Image = Image.FromFile("Resources\\Lenna.png")
+            });
+        }
+
+    }
+
+}
--- Lappi/Program.cs
+++ Lappi/Program.cs
@@ -9,7 +9,7 @@ namespace Lappi {
         private static void Main () {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new MainForm());
         }
 
     }
