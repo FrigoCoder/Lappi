48641ba9a639ebd280cbb6d009d004bce11d2164
Frigo <frigocoder@gmail.com>
2017-10-03 19:38:13 +0200

    RGB8: Added test.

---
+++ LappiTest/RGB8Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+using Lappi;
+
+using NUnit.Framework;
+
+namespace LappiTest {
+
+    [TestFixture]
+    public class RGB8Test {
+
+        [Test]
+        public void Conversion_from_Color () {
+            Assert.That(new RGB8(Color.Red), Is.EqualTo(new RGB8(255, 0, 0)));
+            Assert.That(new RGB8(Color.Lime), Is.EqualTo(new RGB8(0, 255, 0)));
+            Assert.That(new RGB8(Color.Blue), Is.EqualTo(new RGB8(0, 0, 255)));
+        }
+
+        [Test]
+        public void Conversion_to_Color () {
+            Assert.That(new RGB8(255, 0, 0).ToColor().ToArgb(), Is.EqualTo(Color.Red.ToArgb()));
+            Assert.That(new RGB8(0, 255, 0).ToColor().ToArgb(), Is.EqualTo(Color.Lime.ToArgb()));
+            Assert.That(new RGB8(0, 0, 255).ToColor().ToArgb(), Is.EqualTo(Color.Blue.ToArgb()));
+        }
+
+    }
+
+}
