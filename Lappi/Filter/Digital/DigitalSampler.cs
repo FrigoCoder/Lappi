d58c99d9817c9d68acce5ae60d1315fbc0596b3e
Frigo <frigocoder@gmail.com>
2017-10-15 22:33:51 +0200

    Lappi.Filter.Digital: Implemented DigitalSampler.

---
+++ Lappi/Filter/Digital/DigitalSampler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lappi.Filter.Digital {
+
+    public class DigitalSampler {
+
+        private readonly DigitalFilter filter;
+
+        public DigitalSampler (DigitalFilter filter) {
+            this.filter = filter;
+        }
+
+        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
+        public double Sample (double[] v, int center) {
+            double result = 0;
+            double sum = 0;
+            int left = Math.Max(center + filter.Left, 0);
+            int right = Math.Min(center + filter.Right, v.Length - 1);
+            Func<int, double> function = filter.Function;
+            for( int index = left; index <= right; index++ ) {
+                double weight = function(index - center);
+                result += v[index] * weight;
+                sum += Math.Abs(weight);
+            }
+            return sum == 0 ? 0 : result / sum;
+        }
+
+    }
+
+}
---
+++ LappiTest/Filter/Digital/DigitalSamplerTest.cs
@@ -0,0 +1,44 @@
+﻿using Lappi.Filter.Analog;
+using Lappi.Filter.Digital;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    [TestFixture]
+    public class DigitalSamplerTest {
+
+        private readonly double[] source = {1, 4, 9, 16, 25, 36};
+
+        [TestCase]
+        public void Sampling_with_scale_1_linear_filter_should_return_array_value () {
+            DigitalSampler sampler = new DigitalSampler(new DigitalAdapter(new Linear(), 1.0));
+            for( int i = 0; i < source.Length; i++ ) {
+                Assert.That(sampler.Sample(source, i), Is.EqualTo(source[i]));
+            }
+        }
+
+        [TestCase]
+        public void Sampling_with_scale_2_linear_filter_should_blur_linearly () {
+            DigitalSampler sampler = new DigitalSampler(new DigitalAdapter(new Linear(), 2.0));
+            double[] expected = {2, 4.5, 9.5, 16.5, 25.5, 32.333333333333333333333333333333};
+            for( int i = 0; i < source.Length; i++ ) {
+                Assert.That(sampler.Sample(source, i), Is.EqualTo(expected[i]));
+            }
+        }
+
+        [TestCase]
+        public void Sampling_beyond_left_boundary_returns_0 () {
+            DigitalSampler sampler = new DigitalSampler(new DigitalAdapter(new Linear(), 2.0));
+            Assert.That(sampler.Sample(source, -2), Is.EqualTo(0));
+        }
+
+        [TestCase]
+        public void Sampling_beyond_right_boundary_returns_0 () {
+            DigitalSampler sampler = new DigitalSampler(new DigitalAdapter(new Linear(), 2.0));
+            Assert.That(sampler.Sample(source, source.Length + 1), Is.EqualTo(0));
+        }
+
+    }
+
+}
