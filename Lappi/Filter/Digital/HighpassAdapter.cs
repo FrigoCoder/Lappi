bf148301dca3a5bfe831bbf9be5806d0ccc1f0df
Frigo <frigocoder@gmail.com>
2017-10-23 18:46:54 +0200

    Added HighpassAdapter.

---
+++ Lappi/Filter/Digital/HighpassAdapter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lappi.Filter.Digital {
+
+    public class HighpassAdapter : DigitalFilter {
+
+        public int Left { get; }
+        public int Right { get; }
+        public int Radius { get; }
+        public Func<int, double> Kernel => x => coefficients[x - Left];
+        private readonly double[] coefficients;
+
+        public HighpassAdapter (DigitalFilter filter) {
+            Left = filter.Left;
+            Right = filter.Right;
+            Radius = filter.Radius;
+            coefficients = CreateCoefficients(filter);
+        }
+
+        private static double[] CreateCoefficients (DigitalFilter filter) {
+            double[] coefficients = new double[filter.Right - filter.Left + 1];
+            double sum = Normalize(filter);
+            for( int i = 0; i < coefficients.Length; i++ ) {
+                coefficients[i] = -filter.Kernel(i + filter.Left) / sum;
+            }
+            coefficients[-filter.Left] = 1 - filter.Kernel(0) / sum;
+            return coefficients;
+        }
+
+        private static double Normalize (DigitalFilter filter) {
+            double sum = 0;
+            for( int i = filter.Left; i <= filter.Right; i++ ) {
+                sum += filter.Kernel(i);
+            }
+            return sum;
+        }
+
+    }
+
+}
---
+++ LappiTest/Filter/Digital/HighpassAdapterTest.cs
@@ -0,0 +1,31 @@
+﻿using Lappi.Filter.Analog;
+using Lappi.Filter.Digital;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    [TestFixture]
+    public class HighpassAdapterTest {
+
+        [TestCase]
+        public void Highpass_of_linear_filter () {
+            DigitalFilter highpass = new HighpassAdapter(new DigitalAdapter(new Linear(), 2.0));
+            double[] expected = {-0.25, 0.5, -0.25};
+            FilterTestUtil.AssertCoefficients(highpass, expected);
+        }
+
+        [TestCase]
+        public void Highpass_of_dirichlet_filter () {
+            DigitalFilter highpass = new HighpassAdapter(new DigitalAdapter(new Dirichlet(4), 2.0));
+            double[] expected = {
+                0.0124320229612286, 0.0000000000000000, -0.0417611648699562, 0.0000000000000000, 0.0935378601665931, 0.0000000000000000,
+                -0.3142087182578655, 0.5000000000000000, -0.3142087182578655, 0.0000000000000000, 0.0935378601665930, 0.0000000000000000,
+                -0.0417611648699562, 0.0000000000000000, 0.0124320229612286
+            };
+            FilterTestUtil.AssertCoefficients(highpass, expected);
+        }
+
+    }
+
+}
