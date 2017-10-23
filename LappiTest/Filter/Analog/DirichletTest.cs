378c266c86741c0b8592b8892bfc4cf4c8b0a374
Frigo <frigocoder@gmail.com>
2017-10-23 20:34:00 +0200

    Got rid of FilterTestUtil.

--- LappiTest/Filter/Analog/DirichletTest.cs
+++ LappiTest/Filter/Analog/DirichletTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using Lappi.Filter.Analog;
+using Lappi.Filter.Digital;
 
 using NUnit.Framework;
 
@@ -11,15 +12,15 @@ namespace LappiTest.Filter.Analog {
 
         [TestCase]
         public void Dirichlet_downsampling_coefficients_are_appropriate () {
-            FilterTestUtil.AssertCoefficients(new Dirichlet(1), 2.0, new[] {0.5, 1.0, 0.5});
-            FilterTestUtil.AssertCoefficients(new Dirichlet(2), 2.0,
+            AssertCoefficients(new Dirichlet(1), 2.0, new[] {0.5, 1.0, 0.5});
+            AssertCoefficients(new Dirichlet(2), 2.0,
                 new[] {-0.10355339059327377, 0, 0.60355339059327384, 1.0, 0.60355339059327384, 0, -0.10355339059327377});
-            FilterTestUtil.AssertCoefficients(new Dirichlet(3), 2.0,
+            AssertCoefficients(new Dirichlet(3), 2.0,
                 new[] {
                     0.044658198738520435, 0, -0.16666666666666666, 0, 0.62200846792814635, 1.0, 0.62200846792814635, 0, -0.16666666666666666, 0,
                     0.044658198738520435
                 });
-            FilterTestUtil.AssertCoefficients(new Dirichlet(4), 2.0,
+            AssertCoefficients(new Dirichlet(4), 2.0,
                 new[] {
                     -0.024864045922457247, 0, 0.083522329739912374, 0, -0.18707572033318615, 0, 0.62841743651573101, 1.0, 0.62841743651573101, 0,
                     -0.18707572033318615, 0, 0.083522329739912374, 0, -0.024864045922457247
@@ -28,10 +29,10 @@ namespace LappiTest.Filter.Analog {
 
         [TestCase]
         public void Dirichlet_same_as_naive_implementation () {
-            FilterTestUtil.AssertFiltersEqual(new Dirichlet(1), new DirichletNaive(1), 0.125);
-            FilterTestUtil.AssertFiltersEqual(new Dirichlet(2), new DirichletNaive(2), 0.125);
-            FilterTestUtil.AssertFiltersEqual(new Dirichlet(3), new DirichletNaive(3), 0.125);
-            FilterTestUtil.AssertFiltersEqual(new Dirichlet(4), new DirichletNaive(4), 0.125);
+            AssertFiltersEqual(new Dirichlet(1), new DirichletNaive(1), 0.125);
+            AssertFiltersEqual(new Dirichlet(2), new DirichletNaive(2), 0.125);
+            AssertFiltersEqual(new Dirichlet(3), new DirichletNaive(3), 0.125);
+            AssertFiltersEqual(new Dirichlet(4), new DirichletNaive(4), 0.125);
         }
 
         private class DirichletNaive : AnalogFilter, ResamplingFilter {
@@ -61,6 +62,19 @@ namespace LappiTest.Filter.Analog {
                 this.n = n;
             }
 
+        }
+
+        private static void AssertCoefficients (AnalogFilter filter, double scale, double[] coeffs) {
+            Assert.That(new DigitalAdapter(filter, scale).Coefficients, Is.EqualTo(coeffs).Within(1E-15));
+        }
+
+        private static void AssertFiltersEqual (AnalogFilter actual, AnalogFilter expected, double granularity) {
+            Assert.That(actual.Left, Is.EqualTo(expected.Left));
+            Assert.That(actual.Right, Is.EqualTo(expected.Right));
+            Assert.That(actual.Radius, Is.EqualTo(expected.Radius));
+            for( double x = actual.Left - granularity; x <= actual.Right + granularity; x += granularity ) {
+                Assert.That(actual.Kernel(x), Is.EqualTo(expected.Kernel(x)).Within(1E-15));
+            }
         }
 
     }
