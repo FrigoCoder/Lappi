cf005eccbe67f2446012c0f5b93ca775acaa6e02
Frigo <frigocoder@gmail.com>
2017-10-25 00:14:34 +0200

    Lappi: Added IntegralArray.

---
+++ Lappi/Filter/Digital/IntegralArray.cs
@@ -0,0 +1,20 @@
+﻿namespace Lappi.Filter.Digital {
+
+    public class IntegralArray<T> {
+
+        private readonly T[] integral;
+
+        public IntegralArray (T[] array) {
+            integral = new T[array.Length + 1];
+            for( int i = 1; i < integral.Length; i++ ) {
+                integral[i] = (dynamic) integral[i - 1] + array[i - 1];
+            }
+        }
+
+        public T Sum (int left, int right) {
+            return (dynamic) integral[right + 1] - integral[left];
+        }
+
+    }
+
+}
---
+++ LappiTest/Filter/Digital/IntegralArrayTest.cs
@@ -0,0 +1,21 @@
+﻿using Lappi.Filter.Digital;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    [TestFixture]
+    public class IntegralArrayTest {
+
+
+        [TestCase]
+        public void Sum_works_perfectly () {
+            double[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            IntegralArray<double> integral = new IntegralArray<double>(array);
+            Assert.That(integral.Sum(1, 5), Is.EqualTo(15));
+            Assert.That(integral.Sum(0, 9), Is.EqualTo(45));
+        }
+
+    }
+
+}
