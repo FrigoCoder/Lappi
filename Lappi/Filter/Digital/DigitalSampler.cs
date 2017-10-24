c309185191770b39cb54249ae9108272cf7b5e1e
Frigo <frigocoder@gmail.com>
2017-10-25 00:40:00 +0200

    DigitalSampler: Prebuffering normalization factors. Greatly simplified sampler.

--- Lappi/Filter/Digital/DigitalSampler.cs
+++ Lappi/Filter/Digital/DigitalSampler.cs
@@ -15,19 +15,26 @@ namespace Lappi.Filter.Digital {
 
         private readonly DigitalFilter filter;
         private readonly double[] coefficients;
+        private readonly IntegralArray<double> sums;
+        private readonly IntegralArray<double> altSums;
 
         public DigitalSampler (DigitalFilter filter) {
             this.filter = filter;
-            coefficients = Normalize(filter.Coefficients);
+            coefficients = filter.Coefficients;
+            sums = new IntegralArray<double>(coefficients);
+            altSums = new IntegralArray<double>(coefficients.Select((x, i) => i % 2 == 0 ? x : -x).ToArray());
         }
 
         public T Sample (T[] source, int center) {
-            int left = center + filter.Left;
-            int right = center + filter.Right;
-            if( 0 <= left && right < source.Length ) {
-                return SampleInner(source, left);
+            int shift = center + filter.Left;
+            int left = Math.Max(0, -shift);
+            int right = Math.Min(coefficients.Length - 1, source.Length - 1 - shift);
+            T sum = new T();
+            for( int i = left; i <= right; i++ ) {
+                sum += (dynamic) source[i + shift] * coefficients[i];
             }
-            return SampleBoundary(source, center);
+            sum /= (dynamic) Normalize(left, right);
+            return sum;
         }
 
         public T SampleHighpass (T[] source, int center) {
@@ -71,49 +78,12 @@ namespace Lappi.Filter.Digital {
             return Convolute(v);
         }
 
-        private T SampleInner (T[] source, int left) {
-            T sum = new T();
-            for( int i = 0; i < coefficients.Length; i++ ) {
-                sum += (dynamic) source[left + i] * coefficients[i];
-            }
-            return sum;
-        }
-
-        private T SampleBoundary (T[] source, int center) {
-            int left = Math.Max(center + filter.Left, 0);
-            int right = Math.Min(center + filter.Right, source.Length - 1);
-            T result = new T();
-            for( int index = left; index <= right; index++ ) {
-                double weight = coefficients[index - center - filter.Left];
-                result += (dynamic) source[index] * weight;
-            }
-            result /= (dynamic) Normalize(center, left, right);
-            return result;
-        }
-
         [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
-        private double Normalize (int center, int left, int right) {
-            double sum = 0;
-            for( int index = left; index <= right; index++ ) {
-                sum += coefficients[index - center - filter.Left];
-            }
-            double altSum = 0;
-            for( int index = left; index <= right; index += 2 ) {
-                altSum += coefficients[index - center - filter.Left];
-            }
-            for( int index = left + 1; index <= right; index += 2 ) {
-                altSum -= coefficients[index - center - filter.Left];
-            }
-            double result = Math.Max(Math.Abs(sum), Math.Abs(altSum));
-            return result == 0 ? 1 : result;
-        }
-
-        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
-        private static double[] Normalize (double[] coefficients) {
-            double sum = coefficients.Sum();
-            double altSum = coefficients.Select((x, i) => i % 2 == 0 ? x : -x).Sum();
+        private double Normalize (int left, int right) {
+            double sum = Math.Abs(sums.Sum(left, right));
+            double altSum = Math.Abs(altSums.Sum(left, right));
             double factor = Math.Max(Math.Abs(sum), Math.Abs(altSum));
-            return factor == 0 ? coefficients : coefficients.Select(x => x / factor).ToArray();
+            return factor == 0 ? 1 : factor;
         }
 
     }
