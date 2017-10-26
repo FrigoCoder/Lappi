8a05b5c16a7f74f99f5b05cd0d63d9ba0306aa1f
Frigo <frigocoder@gmail.com>
2017-10-26 22:02:17 +0200

    Wavelet1D: Implemented forward transform.

---
+++ Lappi/Filter/Digital/Wavelet1D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lappi.Filter.Digital {
+
+    public class Wavelet1D {
+
+        private readonly DigitalSampler lowpassEven;
+        private readonly DigitalSampler lowpassOdd;
+
+        public Wavelet1D (DigitalFilter lowpassEven, DigitalFilter lowpassOdd) {
+            this.lowpassEven = new DigitalSampler(lowpassEven);
+            this.lowpassOdd = new DigitalSampler(lowpassOdd);
+        }
+
+        public Tuple<double[], double[]> Transform (double[] source) {
+            double[] lowpass = lowpassEven.Downsample(source, 2, 0);
+            double[] highpass = lowpassOdd.DownsampleHighpass(source, 2, 1);
+            return Tuple.Create(lowpass, highpass);
+        }
+
+    }
+
+}
---
+++ LappiTest/Filter/Digital/Wavelet1DTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Lappi.Filter.Analog;
+using Lappi.Filter.Digital;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    [TestFixture]
+    public class Wavelet1DTest {
+
+        private readonly double[] source = {1, 4, 9, 16, 25, 36};
+        private readonly DigitalFilter filterEven = new DigitalAdapter(new Linear(), 2.0);
+        private readonly DigitalFilter filterOdd = new DigitalAdapter(new Linear(), 2.0);
+        private Wavelet1D wavelet;
+
+        [SetUp]
+        public void SetUp () {
+            wavelet = new Wavelet1D(filterEven, filterOdd);
+        }
+
+        [TestCase]
+        public void Transform_returns_correct_lowpass_vector () {
+            double[] expected = {2, 9.5, 25.5};
+            Tuple<double[], double[]> result = wavelet.Transform(source);
+            Assert.That(result.Item1, Is.EqualTo(expected));
+        }
+
+        [TestCase]
+        public void Transform_returns_correct_highpass_vector () {
+            double[] expected = {-0.5, -0.5, 3.6666666666666666};
+            Tuple<double[], double[]> result = wavelet.Transform(source);
+            Assert.That(result.Item2, Is.EqualTo(expected).Within(1E-14));
+        }
+
+    }
+
+}
