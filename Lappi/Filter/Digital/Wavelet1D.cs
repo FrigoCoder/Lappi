42ec0b5335e8d2d9ceb703ce8f35af8b79884889
Frigo <frigocoder@gmail.com>
2017-10-29 10:52:30 +0100

    Fuck wavelets, perfect reconstruction, and boundary conditions.

--- Lappi/Filter/Digital/Wavelet1D.cs
+++ Lappi/Filter/Digital/Wavelet1D.cs
@@ -1,21 +1,42 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lappi.Filter.Digital {
 
     public class Wavelet1D {
 
-        private readonly DigitalSampler lowpassEven;
-        private readonly DigitalSampler lowpassOdd;
+        private readonly DigitalSampler analysisLowpass;
+        private readonly DigitalSampler analysisHighpass;
+        private readonly DigitalSampler synthesisLowpass;
+        private readonly DigitalSampler synthesisHighpass;
 
-        public Wavelet1D (DigitalFilter lowpassEven, DigitalFilter lowpassOdd) {
-            this.lowpassEven = new DigitalSampler(lowpassEven);
-            this.lowpassOdd = new DigitalSampler(lowpassOdd);
+        public Wavelet1D (DigitalFilter analysisLowpass, DigitalFilter analysisHighpass) {
+            this.analysisLowpass = new DigitalSampler(analysisLowpass);
+            this.analysisHighpass = new DigitalSampler(analysisHighpass);
+            synthesisLowpass = new DigitalSampler(analysisHighpass);
+            synthesisHighpass = new DigitalSampler(analysisLowpass);
         }
 
         public Tuple<double[], double[]> Transform (double[] source) {
-            double[] lowpass = lowpassEven.Downsample(source, 2, 0);
-            double[] highpass = lowpassOdd.DownsampleHighpass(source, 2, 1);
-            return Tuple.Create(lowpass, highpass);
+            double[] low = analysisLowpass.Downsample(source, 2, 0);
+            double[] high = analysisHighpass.DownsampleHighpass(source, 2, 1);
+            return Tuple.Create(low, high);
+        }
+
+        public double[] Transform (Tuple<double[], double[]> tuple) {
+            double[] result = Transform(tuple.Item1, tuple.Item2);
+            Console.WriteLine(string.Join(", ", result));
+            return result;
+        }
+
+        public double[] Transform (double[] low, double[] high) {
+            double[] v1 = synthesisLowpass.Upsample(low, 2, 1);
+            double[] v2 = synthesisHighpass.UpsampleHighpass(high, 2, 0);
+            double[] result = v1.Zip(v2, (x, y) => 2 * (x + y)).ToArray();
+            Console.WriteLine(string.Join(", ", v1));
+            Console.WriteLine(string.Join(", ", v2));
+            Console.WriteLine(string.Join(", ", result));
+            return result;
         }
 
     }
--- LappiTest/Filter/Digital/Wavelet1DTest.cs
+++ LappiTest/Filter/Digital/Wavelet1DTest.cs
@@ -10,18 +10,13 @@ namespace LappiTest.Filter.Digital {
     [TestFixture]
     public class Wavelet1DTest {
 
-        private readonly double[] source = {1, 4, 9, 16, 25, 36};
-        private readonly DigitalFilter filterEven = new DigitalAdapter(new Linear(), 2.0);
-        private readonly DigitalFilter filterOdd = new DigitalAdapter(new Linear(), 2.0);
-        private Wavelet1D wavelet;
-
-        [SetUp]
-        public void SetUp () {
-            wavelet = new Wavelet1D(filterEven, filterOdd);
-        }
-
         [TestCase]
         public void Transform_returns_correct_lowpass_vector () {
+            double[] source = {1, 4, 9, 16, 25, 36};
+            DigitalFilter lowpass = new DigitalAdapter(new Linear(), 2.0);
+            DigitalFilter highpass = new DigitalAdapter(new Linear(), 2.0);
+            Wavelet1D wavelet = new Wavelet1D(lowpass, highpass);
+
             double[] expected = {2, 9.5, 25.5};
             Tuple<double[], double[]> result = wavelet.Transform(source);
             Assert.That(result.Item1, Is.EqualTo(expected));
@@ -29,9 +24,42 @@ namespace LappiTest.Filter.Digital {
 
         [TestCase]
         public void Transform_returns_correct_highpass_vector () {
+            double[] source = {1, 4, 9, 16, 25, 36};
+            DigitalFilter lowpass = new DigitalAdapter(new Linear(), 2.0);
+            DigitalFilter highpass = new DigitalAdapter(new Linear(), 2.0);
+            Wavelet1D wavelet = new Wavelet1D(lowpass, highpass);
+
             double[] expected = {-0.5, -0.5, 3.6666666666666666};
             Tuple<double[], double[]> result = wavelet.Transform(source);
             Assert.That(result.Item2, Is.EqualTo(expected).Within(1E-14));
+        }
+
+        [Ignore("Fuck wavelets, perfect reconstruction, and boundary conditions"), TestCase]
+        public void Inverse_transform_perfectly_reconstructs_signal_with_CDF_5_3_filters () {
+            double[] source = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144};
+            DigitalFilter lowpass = new CoefficientAdapter(2, new[] {-0.125, 0.25, 0.75, 0.25, -0.125});
+            DigitalFilter highpass = new CoefficientAdapter(1, new[] {0.25, 0.5, 0.25});
+            Wavelet1D wavelet = new Wavelet1D(lowpass, highpass);
+            double[] actual = wavelet.Transform(wavelet.Transform(source));
+            Assert.That(actual, Is.EqualTo(source));
+        }
+
+        [Ignore("Fuck wavelets, perfect reconstruction, and boundary conditions"), TestCase]
+        public void Inverse_transform_perfectly_reconstructs_signal_with_CDF_9_7_filters () {
+            double[] source = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144};
+            DigitalFilter lowpass = new CoefficientAdapter(4,
+                new[] {
+                    0.02674874108097600, -0.01686411844287495, -0.07822326652898785, 0.26686411844287230, 0.60294901823635790, 0.26686411844287230,
+                    -0.07822326652898785, -0.01686411844287495, 0.02674874108097600
+                });
+            DigitalFilter highpass = new CoefficientAdapter(3,
+                new[] {
+                    -0.045635881557124740, -0.028771763114249785, 0.295635881557123500, 0.557543526228497000, 0.295635881557123500,
+                    -0.028771763114249785, -0.045635881557124740
+                });
+            Wavelet1D wavelet = new Wavelet1D(lowpass, highpass);
+            double[] actual = wavelet.Transform(wavelet.Transform(source));
+            Assert.That(actual, Is.EqualTo(source));
         }
 
     }
