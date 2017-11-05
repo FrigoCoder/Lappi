f6fbf6d0391e212b8cc24e535a3ac936c72f421b
Frigo <frigocoder@gmail.com>
2017-11-05 19:39:20 +0100

    Image: Refactored tests.

--- LappiTest/ImageTest.cs
+++ LappiTest/ImageTest.cs
@@ -11,51 +11,50 @@ namespace LappiTest {
     [TestFixture]
     public class ImageTest {
 
-        private Image<Rgb8> image;
         private readonly Random random = new Random();
 
         [Test]
         public void Can_load_640_by_480_white_image () {
-            LoadImage("640x480_white.png");
-            AssertDimensions(640, 480);
-            AssertSolidColor(Color.White);
+            Image<Rgb8> image = LoadImage<Rgb8>("640x480_white.png");
+            AssertDimensions(image, 640, 480);
+            AssertSolidColor(image, Color.White);
         }
 
         [Test]
         public void Can_load_800_by_600_black_image () {
-            LoadImage("800x600_black.png");
-            AssertDimensions(800, 600);
-            AssertSolidColor(Color.Black);
+            Image<Rgb8> image = LoadImage<Rgb8>("800x600_black.png");
+            AssertDimensions(image, 800, 600);
+            AssertSolidColor(image, Color.Black);
         }
 
         [Test]
         public void Can_load_512_by_512_red_image () {
-            LoadImage("512x512_red.png");
-            AssertDimensions(512, 512);
-            AssertSolidColor(Color.Red);
+            Image<Rgb8> image = LoadImage<Rgb8>("512x512_red.png");
+            AssertDimensions(image, 512, 512);
+            AssertSolidColor(image, Color.Red);
         }
 
         [Test]
         public void Can_load_512_by_512_green_image () {
-            LoadImage("512x512_green.png");
-            AssertDimensions(512, 512);
-            AssertSolidColor(Color.Lime);
+            Image<Rgb8> image = LoadImage<Rgb8>("512x512_green.png");
+            AssertDimensions(image, 512, 512);
+            AssertSolidColor(image, Color.Lime);
         }
 
         [Test]
         public void Can_load_512_by_512_blue_image () {
-            LoadImage("512x512_blue.png");
-            AssertDimensions(512, 512);
-            AssertSolidColor(Color.Blue);
+            Image<Rgb8> image = LoadImage<Rgb8>("512x512_blue.png");
+            AssertDimensions(image, 512, 512);
+            AssertSolidColor(image, Color.Blue);
         }
 
         [Test]
         public void Corners_have_correct_coordinates () {
-            LoadImage("black_red_green_blue.png");
-            AssertPixel(0, 0, Color.Black);
-            AssertPixel(1, 0, Color.Red);
-            AssertPixel(0, 1, Color.Lime);
-            AssertPixel(1, 1, Color.Blue);
+            Image<Rgb8> image = LoadImage<Rgb8>("black_red_green_blue.png");
+            AssertPixel(image, 0, 0, Color.Black);
+            AssertPixel(image, 1, 0, Color.Red);
+            AssertPixel(image, 0, 1, Color.Lime);
+            AssertPixel(image, 1, 1, Color.Blue);
         }
 
         [Test]
@@ -100,8 +99,8 @@ namespace LappiTest {
             Assert.That(imageDouble.Columns[0], Is.EqualTo(array));
         }
 
-        private void LoadImage (string filename) {
-            image = Image<Rgb8>.Load("LappiTest\\Resources\\ImageTest\\" + filename);
+        private Image<T> LoadImage<T> (string filename) {
+            return Image<T>.Load("LappiTest\\Resources\\ImageTest\\" + filename);
         }
 
         private Image<Rgb8> CreateRandomImage (int xs, int ys) {
@@ -115,18 +114,18 @@ namespace LappiTest {
             return result;
         }
 
-        private void AssertDimensions (int xs, int ys) {
+        private void AssertDimensions<T> (Image<T> image, int xs, int ys) {
             Assert.That(image.Xs, Is.EqualTo(xs));
             Assert.That(image.Ys, Is.EqualTo(ys));
         }
 
-        private void AssertPixel (int x, int y, Color color) {
-            Rgb8 value = (Rgb8) Activator.CreateInstance(typeof(Rgb8), color);
+        private void AssertPixel<T> (Image<T> image, int x, int y, Color color) {
+            T value = (T) Activator.CreateInstance(typeof(T), color);
             Assert.That(image[x, y], Is.EqualTo(value));
         }
 
-        private void AssertSolidColor (Color color) {
-            Rgb8 value = (Rgb8) Activator.CreateInstance(typeof(Rgb8), color);
+        private void AssertSolidColor<T> (Image<T> image, Color color) {
+            T value = (T) Activator.CreateInstance(typeof(T), color);
             for( int x = 0; x < image.Xs; x++ ) {
                 for( int y = 0; y < image.Ys; y++ ) {
                     Assert.That(image[x, y], Is.EqualTo(value));
@@ -134,7 +133,7 @@ namespace LappiTest {
             }
         }
 
-        private void AssertEquals (Image<Rgb8> expected, Image<Rgb8> actual) {
+        private void AssertEquals<T> (Image<T> expected, Image<T> actual) {
             Assert.That(actual.Xs, Is.EqualTo(expected.Xs));
             Assert.That(actual.Ys, Is.EqualTo(expected.Ys));
             for( int x = 0; x < actual.Xs; x++ ) {
