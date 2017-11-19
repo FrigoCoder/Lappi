c85a19dd59a68de306b8bfdf311e0092ff955d28
Frigo <frigocoder@gmail.com>
2017-11-19 19:37:51 +0100

    Laplacian: Added Filter2D.

---
+++ Lappi/Filter/Digital/Filter2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lappi.Filter.Digital {
+
+    public interface Filter2D {
+
+        int Left { get; }
+        int Right { get; }
+        int Top { get; }
+        int Bottom { get; }
+
+        Func<int, int, double> Kernel { get; }
+
+    }
+
+}
