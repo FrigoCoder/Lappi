e147f5a138649d8269b694ec19e3d3b6fd68abf2
Frigo <frigocoder@gmail.com>
2017-11-19 15:42:51 +0100

    Saving all scales of Lenna.

--- Lappi/Program.cs
+++ Lappi/Program.cs
@@ -15,20 +15,25 @@ namespace Lappi {
 
             //            Application.Run(new MainForm());
 
-            Image<YuvD> image = Image<YuvD>.Load("Resources\\Lenna.png");
+            Image<YuvD> lenna = Image<YuvD>.Load("Resources\\Lenna.png");
+            Laplacian2D<YuvD> transform = new Laplacian2D<YuvD>(CDF97.AnalysisLowpass, CDF97.SynthesisLowpass);
 
-            Laplacian2D<YuvD> laplacian = new Laplacian2D<YuvD>(CDF97.AnalysisLowpass, CDF97.SynthesisLowpass);
-            Image<YuvD>[] tuple = laplacian.Forward(image);
-            Image<YuvD> low = tuple[0];
-            Image<YuvD> high = tuple[1];
-            for( int x = 0; x < high.Xs; x++ ) {
-                for( int y = 0; y < high.Ys; y++ ) {
-                    high[x, y] += new YuvD(0.5, 0, 0);
+            Image<YuvD>[] transformed = transform.Forward(lenna, 5);
+            Image<YuvD> reconstructed = transform.Inverse(transformed);
+
+            for( int i = 1; i < transformed.Length; i++ ) {
+                transformed[i].Normalize();
+                transformed[i].Save($"c:\\temp\\lenna-{i}.png");
+            }
+            reconstructed.Save("c:\\temp\\lenna-r.png");
+        }
+
+        private static void Normalize (this Image<YuvD> image) {
+            for( int x = 0; x < image.Xs; x++ ) {
+                for( int y = 0; y < image.Ys; y++ ) {
+                    image[x, y] += new YuvD(0.5, 0, 0);
                 }
             }
-
-            low.Save("c:\\temp\\low.png");
-            high.Save("c:\\temp\\high.png");
         }
 
     }
