809f7b45e60152a13b31eb2f562ca0a0e891390b
Frigo <frigocoder@gmail.com>
2017-12-03 21:06:02 +0100

    FFT: Implemented FFT algorithm along with tests.

---
+++ Lappi/FFT.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Lappi {
+
+    public class Fft {
+
+        public Complex[] Forward (Complex[] T) {
+            Complex[] F = (Complex[]) T.Clone();
+            Core(F, -1.0);
+            Normalize(F);
+            return F;
+        }
+
+        public Complex[] Inverse (Complex[] F) {
+            Complex[] T = (Complex[]) F.Clone();
+            Core(T, 1.0);
+            return T;
+        }
+
+        private void Core (Complex[] v, double sign) {
+            if( v.Length == 0 || (v.Length & (v.Length - 1)) != 0 ) {
+                throw new ArgumentException("Array length must be power of two");
+            }
+            for( int blockSize = v.Length; blockSize > 1; blockSize /= 2 ) {
+                Complex root = Complex.Cis(sign * 2.0 * Math.PI / blockSize);
+                Complex twiddle = 1;
+                for( int i = 0; i < blockSize / 2; i++ ) {
+                    for( int x1 = i; x1 < v.Length; x1 += blockSize ) {
+                        int x2 = x1 + blockSize / 2;
+                        Complex e = v[x1];
+                        Complex f = v[x2];
+                        v[x1] = e + f;
+                        v[x2] = (e - f) * twiddle;
+                    }
+                    twiddle *= root;
+                }
+            }
+            BitReverse(v);
+        }
+
+        private void BitReverse (Complex[] v) {
+            uint p = NumberOfLeadingZeros((uint) (v.Length - 1));
+            for( uint i = 0; i < v.Length; i++ ) {
+                uint j = Reverse(i) >> (int) p;
+                if( i < j ) {
+                    Swap(v, i, j);
+                }
+            }
+        }
+
+        private uint NumberOfLeadingZeros (uint i) {
+            if( i == 0 ) {
+                return 32;
+            }
+            uint n = 0;
+            if( i >> 16 == 0 ) {
+                n += 16;
+                i <<= 16;
+            }
+            if( i >> 24 == 0 ) {
+                n += 8;
+                i <<= 8;
+            }
+            if( i >> 28 == 0 ) {
+                n += 4;
+                i <<= 4;
+            }
+            if( i >> 30 == 0 ) {
+                n += 2;
+                i <<= 2;
+            }
+            if( i >> 31 == 0 ) {
+                n++;
+            }
+            return n;
+        }
+
+        private uint Reverse (uint i) {
+            i = ((i & 0x55555555) << 1) | ((i >> 1) & 0x55555555);
+            i = ((i & 0x33333333) << 2) | ((i >> 2) & 0x33333333);
+            i = ((i & 0x0f0f0f0f) << 4) | ((i >> 4) & 0x0f0f0f0f);
+            i = ((i & 0x00ff00ff) << 8) | ((i >> 8) & 0x00ff00ff);
+            i = ((i & 0x0000ffff) << 16) | ((i >> 16) & 0x0000ffff);
+            return i;
+        }
+
+        private void Swap (Complex[] v, uint i, uint j) {
+            Complex t = v[i];
+            v[i] = v[j];
+            v[j] = t;
+        }
+
+        private void Normalize (Complex[] F) {
+            for( int f = 0; f < F.Length; f++ ) {
+                F[f] /= F.Length;
+            }
+        }
+
+    }
+
+}
---
+++ LappiTest/FftTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Lappi;
+
+using NUnit.Framework;
+
+namespace LappiTest {
+
+    public class FftTest {
+
+        private readonly int n = 1024;
+        private readonly Random random = new Random();
+        private readonly Fft fft = new Fft();
+
+        [Test]
+        public void Forward_transform_correctly_transforms_constant_signal () {
+            Complex[] T = new Complex[n];
+            for( int t = 0; t < n; t++ ) {
+                T[t] = 1;
+            }
+            Complex[] F = fft.Forward(T);
+            for( int f = 0; f < n; f++ ) {
+                Complex expected = f == 0 ? 1 : 0;
+                Assert.That(F[f], Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void Forward_transform_correctly_transforms_cis_signal () {
+            Complex[] T = new Complex[n];
+            for( int t = 0; t < n; t++ ) {
+                T[t] = Complex.Cis(2 * Math.PI * 10 * t / n);
+            }
+            Complex[] F = fft.Forward(T);
+            for( int f = 0; f < n; f++ ) {
+                Complex expected = f == 10 ? 1 : 0;
+                Assert.That(F[f], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
+            }
+        }
+
+        [Test]
+        public void Inverse_transform_correctly_restores_constant_signal () {
+            Complex[] F = new Complex[n];
+            F[0] = 1;
+            Complex[] T = fft.Inverse(F);
+            for( int t = 0; t < n; t++ ) {
+                Complex expected = 1;
+                Assert.That(T[t], Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void Inverse_transform_correctly_restores_cis_signal () {
+            Complex[] F = new Complex[n];
+            F[10] = 1;
+            Complex[] T = fft.Inverse(F);
+            for( int t = 0; t < n; t++ ) {
+                Complex expected = Complex.Cis(2 * Math.PI * 10 * t / n);
+                Assert.That(T[t], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
+            }
+        }
+
+        [Test]
+        public void Forward_transform_followed_by_inverse_transform_correctly_restores_random_array () {
+            Complex[] T = new Complex[n];
+            for( int t = 0; t < T.Length; t++ ) {
+                T[t] = new Complex(random.NextDouble(), random.NextDouble());
+            }
+            Assert.That(fft.Inverse(fft.Forward(T)), Is.EqualTo(T).Using(Complex.Within(1E-16)));
+        }
+
+    }
+
+}
