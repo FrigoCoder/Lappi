0e6efda026af176b7914e6bba98441d4016c2428
Frigo <frigocoder@gmail.com>
2017-12-15 20:29:59 +0100

    Leftover.

--- Lappi/Program.cs
+++ Lappi/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 
-using Lappi.Filter.Analog;
 using Lappi.Filter.Digital;
 using Lappi.Image;
 
@@ -16,18 +15,33 @@ namespace Lappi {
 
             Image<YuvD> lenna = Image<YuvD>.Load("Resources\\Lenna.png");
 
-            Filter2D analysis = new RadialAdapter(new Dirichlet(4), 2.0);
-            Filter2D synthesis = new RadialAdapter(new Dirichlet(3), 2.0);
-            Laplacian2D<YuvD> transform = new Laplacian2D<YuvD>(analysis, synthesis);
+            /*            for( int a = 1; a <= 8; a++ ) {
+                            for( int s = 1; s <= 8; s++ ) {
+                                DigitalFilter analysis = new DigitalAdapter(new Dirichlet(a), 2.0);
+                                DigitalFilter synthesis = new DigitalAdapter(new Dirichlet(s), 2.0);
+                                LaplacianSeparable<YuvD> transform = new LaplacianSeparable<YuvD>(analysis, synthesis);
 
-/*            DigitalFilter analysis = new DigitalAdapter(new Dirichlet(4), 2.0);
-            DigitalFilter synthesis = new DigitalAdapter(new Dirichlet(3), 2.0);
-            LaplacianSeparable<YuvD> transform = new LaplacianSeparable<YuvD>(analysis, synthesis);*/
+                                Image<YuvD>[] transformed = transform.Forward(lenna, 5);
+                                Image<YuvD> reconstructed = transform.Inverse(transformed);
+
+                                transformed[5].Normalize();
+                                transformed[5].Save($"c:\\temp\\lenna-{a}_{s}.png");
+                                Console.WriteLine($"c:\\temp\\lenna-{a}_{s}.png");
+                            }
+                        }
+            */
+
+            //                        DigitalFilter analysis = new DigitalAdapter(new Dirichlet(3), 2.0);
+            //                        DigitalFilter synthesis = new DigitalAdapter(new Dirichlet(3), 2.0);
+
+            DigitalFilter analysis = new CoefficientAdapter(4, new[] {0.015625, 0, -0.125, 0.25, 0.71875, 0.25, -0.125, 0, 0.015625});
+            DigitalFilter synthesis = new CoefficientAdapter(3, new[] {-0.03125, 0, 0.28125, 0.5, 0.28125, 0, -0.03125});
+            LaplacianSeparable<YuvD> transform = new LaplacianSeparable<YuvD>(analysis, synthesis);
 
             Image<YuvD>[] transformed = transform.Forward(lenna, 5);
             Image<YuvD> reconstructed = transform.Inverse(transformed);
 
-            transformed[0].Save($"c:\\temp\\lenna-0.png");
+            transformed[0].Save("c:\\temp\\lenna-0.png");
             for( int i = 1; i < transformed.Length; i++ ) {
                 transformed[i].Normalize();
                 transformed[i].Save($"c:\\temp\\lenna-{i}.png");
