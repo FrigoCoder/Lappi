106e39d17985621013bd6d71c4186d5dcb1b150c
Frigo <frigocoder@gmail.com>
2017-12-24 17:59:18 +0100

    Image: Introduced default value constructor.

--- Lappi/Filter/Digital2D/DigitalSampler2D.cs
+++ Lappi/Filter/Digital2D/DigitalSampler2D.cs
@@ -50,14 +50,7 @@ namespace Lappi.Filter.Digital2D {
         public Image<T> Upsample (Image<T> source, int factor, int shift) => Upsample(source, factor, shift, source.Xs * factor, source.Ys * factor);
 
         public Image<T> Upsample (Image<T> source, int factor, int shift, int xs, int ys) {
-            Image<T> result = new Image<T>(xs, ys);
-            if( default(T) == null ) {
-                for( int x = 0; x < result.Xs; x++ ) {
-                    for( int y = 0; y < result.Ys; y++ ) {
-                        result[x, y] = new T();
-                    }
-                }
-            }
+            Image<T> result = new Image<T>(xs, ys, new T());
             for( int x = 0; x < source.Xs; x++ ) {
                 for( int y = 0; y < source.Ys; y++ ) {
                     result[x * factor + shift, y * factor + shift] = (dynamic) source[x, y] * factor * factor;
--- Lappi/Image/Image.cs
+++ Lappi/Image/Image.cs
@@ -60,6 +60,14 @@ namespace Lappi.Image {
         public Image (int xs, int ys) : this(new T[ys, xs]) {
         }
 
+        public Image (int xs, int ys, T defaultValue) : this(new T[ys, xs]) {
+            for( int x = 0; x < xs; x++ ) {
+                for( int y = 0; y < ys; y++ ) {
+                    this[x, y] = defaultValue;
+                }
+            }
+        }
+
         public Image (T[,] pixels) {
             Xs = pixels.GetLength(1);
             Ys = pixels.GetLength(0);
