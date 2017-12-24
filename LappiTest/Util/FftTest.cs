6d7f99323f8d541dca0b7c620b88e3c8bf8793fb
Frigo <frigocoder@gmail.com>
2017-12-24 20:53:07 +0100

    Greatly simplified FFT tests.

--- LappiTest/Util/FftTest.cs
+++ LappiTest/Util/FftTest.cs
@@ -17,129 +17,62 @@ namespace LappiTest.Util {
         /// </summary>
         [Test]
         public void Simple_example () {
-            Complex[] T = new Complex[8];
-            T[1] = 1;
-            Complex[] expected = {
+            Complex[] time = Arrays.New<Complex>(8, t => t == 1 ? 1 : 0);
+            Complex[] freq = {
                 new Complex(0.125, 0), new Complex(0.088, -0.088), new Complex(0, -0.125), new Complex(-0.088, -0.088), new Complex(-0.125, 0),
                 new Complex(-0.088, 0.088), new Complex(0, 0.125), new Complex(0.088, 0.088)
             };
-            Complex[] F = fft.Forward(T);
-            Assert.That(F, Is.EqualTo(expected).Using(Complex.Within(1E-6)));
+            Assert.That(fft.Forward(time), Is.EqualTo(freq).Using(Complex.Within(1E-6)));
         }
 
         [Test]
-        public void Full_transform_correctly_restores_random_array () {
+        public void Random_signal () {
             for( int n = 1; n <= maxn; n *= 2 ) {
-                Complex[] T = new Complex[n];
-                T.Fill(i => new Complex(random.NextDouble(), random.NextDouble()));
-                Assert.That(fft.Inverse(fft.Forward(T)), Is.EqualTo(T).Using(Complex.Within(1E-16)));
+                Complex[] time = Arrays.New(n, t => new Complex(random.NextDouble(), random.NextDouble()));
+                Complex[] freq = fft.Forward(time);
+                AssertTimeFreq(time, freq);
             }
         }
 
         [Test]
-        public void Forward_transform_correctly_transforms_constant_signal () {
+        public void Constant_signal () {
             for( int n = 1; n <= maxn; n *= 2 ) {
-                Complex[] T = new Complex[n];
-                T.Fill(i => 1);
-                Complex[] F = fft.Forward(T);
-                for( int f = 0; f < n; f++ ) {
-                    Complex expected = f == 0 ? 1 : 0;
-                    Assert.That(F[f], Is.EqualTo(expected));
-                }
+                Complex[] time = Arrays.New<Complex>(n, t => 1);
+                Complex[] freq = Arrays.New<Complex>(n, f => f == 0 ? 1 : 0);
+                AssertTimeFreq(time, freq);
             }
         }
 
         [Test]
-        public void Inverse_transform_correctly_restores_constant_signal () {
-            for( int n = 1; n <= maxn; n *= 2 ) {
-                Complex[] F = new Complex[n];
-                F[0] = 1;
-                Complex[] T = fft.Inverse(F);
-                for( int t = 0; t < n; t++ ) {
-                    Complex expected = 1;
-                    Assert.That(T[t], Is.EqualTo(expected));
-                }
-            }
-        }
-
-        [Test]
-        public void Forward_transform_correctly_transforms_cis_signal () {
+        public void Cis_signal () {
             for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] T = new Complex[n];
-                T.Fill(t => Complex.Cis(2 * Math.PI * 10 * t / n));
-                Complex[] F = fft.Forward(T);
-                for( int f = 0; f < n; f++ ) {
-                    Complex expected = f == 10 ? 1 : 0;
-                    Assert.That(F[f], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
+                Complex[] time = Arrays.New(n, t => Complex.Cis(2 * Math.PI * 10 * t / n));
+                Complex[] freq = Arrays.New<Complex>(n, f => f == 10 ? 1 : 0);
+                AssertTimeFreq(time, freq);
             }
         }
 
         [Test]
-        public void Inverse_transform_correctly_restores_cis_signal () {
+        public void Cos_signal () {
             for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] F = new Complex[n];
-                F[10] = 1;
-                Complex[] T = fft.Inverse(F);
-                for( int t = 0; t < n; t++ ) {
-                    Complex expected = Complex.Cis(2 * Math.PI * 10 * t / n);
-                    Assert.That(T[t], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
+                Complex[] time = Arrays.New<Complex>(n, t => Math.Cos(2 * Math.PI * 10 * t / n));
+                Complex[] freq = Arrays.New<Complex>(n, f => f == 10 ? 0.5 : f == n - 10 ? 0.5 : 0);
+                AssertTimeFreq(time, freq);
             }
         }
 
         [Test]
-        public void Forward_transform_correctly_transforms_cos_signal () {
+        public void Sin_signal () {
             for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] T = new Complex[n];
-                T.Fill(t => Math.Cos(2 * Math.PI * 10 * t / n));
-                Complex[] F = fft.Forward(T);
-                for( int f = 0; f < n; f++ ) {
-                    Complex expected = f == 10 ? 0.5 : f == n - 10 ? 0.5 : 0;
-                    Assert.That(F[f], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
+                Complex[] time = Arrays.New<Complex>(n, t => Math.Sin(2 * Math.PI * 10 * t / n));
+                Complex[] freq = Arrays.New(n, f => f == 10 ? new Complex(0, -0.5) : f == n - 10 ? new Complex(0, 0.5) : 0);
+                AssertTimeFreq(time, freq);
             }
         }
 
-        [Test]
-        public void Inverse_transform_correctly_restores_cos_signal () {
-            for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] F = new Complex[n];
-                F[10] = 0.5;
-                F[n - 10] = 0.5;
-                Complex[] T = fft.Inverse(F);
-                for( int t = 0; t < n; t++ ) {
-                    Complex expected = Math.Cos(2 * Math.PI * 10 * t / n);
-                    Assert.That(T[t], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
-            }
-        }
-
-        [Test]
-        public void Inverse_transform_correctly_restores_sin_signal () {
-            for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] F = new Complex[n];
-                F[10] = new Complex(0, -0.5);
-                F[n - 10] = new Complex(0, 0.5);
-                Complex[] T = fft.Inverse(F);
-                for( int t = 0; t < n; t++ ) {
-                    Complex expected = Math.Sin(2 * Math.PI * 10 * t / n);
-                    Assert.That(T[t], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
-            }
-        }
-
-        [Test]
-        public void Forward_transform_correctly_transforms_sin_signal () {
-            for( int n = 16; n <= maxn; n *= 2 ) {
-                Complex[] T = new Complex[n];
-                T.Fill(t => Math.Sin(2 * Math.PI * 10 * t / n));
-                Complex[] F = fft.Forward(T);
-                for( int f = 0; f < n; f++ ) {
-                    Complex expected = f == 10 ? new Complex(0, -0.5) : f == n - 10 ? new Complex(0, 0.5) : 0;
-                    Assert.That(F[f], Is.EqualTo(expected).Using(Complex.Within(1E-16)));
-                }
-            }
+        private void AssertTimeFreq (Complex[] time, Complex[] freq) {
+            Assert.That(fft.Forward(time), Is.EqualTo(freq).Using(Complex.Within(1E-16)));
+            Assert.That(fft.Inverse(freq), Is.EqualTo(time).Using(Complex.Within(1E-16)));
         }
 
     }
