6994ca16756f925953fb67f7e85aaedf0485eeb7
Frigo <frigocoder@gmail.com>
2017-12-25 01:19:26 +0100

    Removed unnecessary method.

--- Lappi/Image/Colors.cs
+++ Lappi/Image/Colors.cs
@@ -7,8 +7,6 @@ namespace Lappi.Image {
 
         public static T To<T> (Color color) => (T) Activator.CreateInstance(typeof(T), color);
 
-        public static T ToT<T> (this Color color) => (T) Activator.CreateInstance(typeof(T), color);
-
     }
 
 }
