63ec347e2bae5dc2e437e2cafaf3ac1428b4ad74
Frigo <frigocoder@gmail.com>
2017-12-31 22:14:22 +0100

    Added YuvD specializations for Dirichlet samplers.

--- Lappi/Filter/Digital/Dirichlet2Sampler.cs
+++ Lappi/Filter/Digital/Dirichlet2Sampler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using Lappi.Image;
 using Lappi.Util;
 
 namespace Lappi.Filter.Digital {
@@ -41,6 +42,47 @@ namespace Lappi.Filter.Digital {
                 v[i * factor + shift] = (dynamic) source[i] * factor;
             }
             return Arrays.New(length, i => i % 2 == 0 ? (T) ((dynamic) v[i] * 0.5) : Sample(v, i));
+        }
+
+    }
+
+    public class Dirichlet2SamplerYuvD : Sampler1D<YuvD> {
+
+        public YuvD Sample (YuvD[] source, int center) {
+            YuvD result = source[center] * 0.5;
+            if( center == source.Length - 1 ) {
+                result += source[center - 1] * 0.5;
+                return result;
+            }
+            switch( Math.Min(center, source.Length - 1 - center) ) {
+                case 0:
+                    result += source[center + 1] * 0.5;
+                    return result;
+                case 1:
+                case 2:
+                    result += (source[center - 1] + source[center + 1]) * 0.25;
+                    return result;
+                default:
+                    result += (source[center - 1] + source[center + 1]) * 0.25;
+                    result += (source[center - 1] + source[center + 1] - source[center - 3] - source[center + 3]) * 0.0517766952966369;
+                    return result;
+            }
+        }
+
+        public YuvD[] Convolute (YuvD[] source) => Arrays.New(source.Length, i => Sample(source, i));
+
+        public YuvD[] Downsample (YuvD[] source, int factor, int shift) =>
+            Arrays.New((source.Length - shift + factor - 1) / factor, i => Sample(source, i * factor + shift));
+
+        public YuvD[] Upsample (YuvD[] source, int factor, int shift) => Upsample(source, factor, shift, source.Length * factor);
+
+        public YuvD[] Upsample (YuvD[] source, int factor, int shift, int length) {
+            Preconditions.Require(factor == 2 && shift == 0);
+            YuvD[] v = Arrays.New(length, new YuvD());
+            for( int i = 0; i < source.Length; i++ ) {
+                v[i * factor + shift] = source[i] * factor;
+            }
+            return Arrays.New(length, i => i % 2 == 0 ? v[i] * 0.5 : Sample(v, i));
         }
 
     }
--- Lappi/Filter/Digital/Dirichlet3Sampler.cs
+++ Lappi/Filter/Digital/Dirichlet3Sampler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using Lappi.Image;
 using Lappi.Util;
 
 namespace Lappi.Filter.Digital {
@@ -47,6 +48,53 @@ namespace Lappi.Filter.Digital {
                 v[i * factor + shift] = (dynamic) source[i] * factor;
             }
             return Arrays.New(length, i => i % 2 == 0 ? (T) ((dynamic) v[i] * 0.5) : Sample(v, i));
+        }
+
+    }
+
+    public class Dirichlet3SamplerYuvD : Sampler1D<YuvD> {
+
+        public YuvD Sample (YuvD[] source, int center) {
+            YuvD result = source[center] * 0.5;
+            if( center == source.Length - 1 ) {
+                result += source[center - 1] * 0.5;
+                return result;
+            }
+            switch( Math.Min(center, source.Length - 1 - center) ) {
+                case 0:
+                    result += source[center + 1] * 0.5;
+                    return result;
+                case 1:
+                case 2:
+                    result += (source[center - 1] + source[center + 1]) * 0.25;
+                    return result;
+                case 3:
+                case 4:
+                    result += (source[center - 1] + source[center + 1]) * 0.25;
+                    result += (source[center - 1] + source[center + 1] - source[center - 3] - source[center + 3]) * 0.0517766952966369;
+                    return result;
+                default:
+                    result += (source[center - 1] + source[center + 1]) * 0.311004233964073;
+                    result += (source[center - 3] + source[center + 3]) * -0.0833333333333333;
+                    result += (source[center - 5] + source[center + 5]) * 0.0223290993692602;
+                    return result;
+            }
+        }
+
+        public YuvD[] Convolute (YuvD[] source) => Arrays.New(source.Length, i => Sample(source, i));
+
+        public YuvD[] Downsample (YuvD[] source, int factor, int shift) =>
+            Arrays.New((source.Length - shift + factor - 1) / factor, i => Sample(source, i * factor + shift));
+
+        public YuvD[] Upsample (YuvD[] source, int factor, int shift) => Upsample(source, factor, shift, source.Length * factor);
+
+        public YuvD[] Upsample (YuvD[] source, int factor, int shift, int length) {
+            Preconditions.Require(factor == 2 && shift == 0);
+            YuvD[] v = Arrays.New(length, new YuvD());
+            for( int i = 0; i < source.Length; i++ ) {
+                v[i * factor + shift] = source[i] * factor;
+            }
+            return Arrays.New(length, i => i % 2 == 0 ? v[i] * 0.5 : Sample(v, i));
         }
 
     }
