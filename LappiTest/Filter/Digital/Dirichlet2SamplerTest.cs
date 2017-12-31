2c39b623df3e77e0e423457f4b89f0791484d295
Frigo <frigocoder@gmail.com>
2017-12-31 21:01:40 +0100

    Added tests for Dirichlet samplers.

---
+++ LappiTest/Filter/Digital/Dirichlet2SamplerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Lappi.Filter.Digital;
+using Lappi.Util;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    using Sampler1D = Sampler1D<double>;
+    using DigitalSampler = DigitalSampler<double>;
+    using Dirichlet2Sampler = Dirichlet2Sampler<double>;
+
+    public class Dirichlet2SamplerTest {
+
+        private readonly double[] array = Arrays.New(1024, i => random.NextDouble());
+        private readonly Sampler1D dirichletSampler = new Dirichlet2Sampler();
+        private readonly Sampler1D digitalSampler = new DigitalSampler(new DirichletBoundaryHandler(2));
+
+        [Test]
+        public void Convolute_test () {
+            Assert.That(dirichletSampler.Convolute(array), Is.EqualTo(digitalSampler.Convolute(array)).Within(1E-15));
+        }
+
+        [Test]
+        public void Downsample_test () {
+            Assert.That(dirichletSampler.Downsample(array, 2, 0), Is.EqualTo(digitalSampler.Downsample(array, 2, 0)).Within(1E-15));
+        }
+
+        [Test]
+        public void Upsample_test () {
+            Assert.That(dirichletSampler.Upsample(array, 2, 0), Is.EqualTo(digitalSampler.Upsample(array, 2, 0)).Within(1E-15));
+        }
+
+        private static readonly Random random = new Random();
+
+    }
+
+}
---
+++ LappiTest/Filter/Digital/Dirichlet3SamplerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Lappi.Filter.Digital;
+using Lappi.Util;
+
+using NUnit.Framework;
+
+namespace LappiTest.Filter.Digital {
+
+    using Sampler1D = Sampler1D<double>;
+    using DigitalSampler = DigitalSampler<double>;
+    using Dirichlet3Sampler = Dirichlet3Sampler<double>;
+
+    public class Dirichlet3SamplerTest {
+
+        private readonly double[] array = Arrays.New(1024, i => random.NextDouble());
+        private readonly Sampler1D dirichletSampler = new Dirichlet3Sampler();
+        private readonly Sampler1D digitalSampler = new DigitalSampler(new DirichletBoundaryHandler(3));
+
+        [Test]
+        public void Convolute_test () {
+            Assert.That(dirichletSampler.Convolute(array), Is.EqualTo(digitalSampler.Convolute(array)).Within(1E-15));
+        }
+
+        [Test]
+        public void Downsample_test () {
+            Assert.That(dirichletSampler.Downsample(array, 2, 0), Is.EqualTo(digitalSampler.Downsample(array, 2, 0)).Within(1E-15));
+        }
+
+        [Test]
+        public void Upsample_test () {
+            Assert.That(dirichletSampler.Upsample(array, 2, 0), Is.EqualTo(digitalSampler.Upsample(array, 2, 0)).Within(1E-15));
+        }
+
+        private static readonly Random random = new Random();
+
+    }
+
+}
